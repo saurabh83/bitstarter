641a87adc135b557b0005b5f915335db3a83d166
saurabh83 <splitt@gmail.com>
2013-07-14 16:48:18 +0000

    Assignment 3 part 3

--- grader.js
+++ grader.js
@@ -24,9 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,10 +65,29 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson;
+    if(program.url) {
+        rest.get(program.url).on('complete', function(result) {
+	    if (result instanceof Error) {
+    		sys.puts('Error: ' + result.message);
+    		this.retry(5000); // try again after 5 sec
+  	    } else {
+		var outfile = "temp.html";
+    		fs.writeFileSync(outfile, result);
+		console.log("Fetched URL\n");
+        	checkJson = checkHtmlFile(outfile, program.checks);
+    		var outJson = JSON.stringify(checkJson, null, 4);
+		    console.log(outJson);
+//		fs.unlink(outfile);
+  	    }
+	    });
+    } else {
+        checkJson = checkHtmlFile(program.file, program.checks);
+    	var outJson = JSON.stringify(checkJson, null, 4);
+    	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }

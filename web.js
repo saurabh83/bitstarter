d820b761be498e0c6ffbef67420cc4c34db1270c
saurabh83 <splitt@gmail.com>
2013-07-14 08:24:50 +0000

    First edit for assignment

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ var buff = new Buffer(50);
+ buff = fs.readFileSync('index.html');
+ 
+ response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
